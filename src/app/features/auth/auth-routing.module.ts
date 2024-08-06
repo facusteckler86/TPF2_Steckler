8f6ee83865ff7aa18c3295374855c63211221f98
josue baez <josuebaez61@gmail.com>
2024-08-05 22:39:14 -0300

    FIX, navigation

--- src/app/features/auth/auth-routing.module.ts
+++ src/app/features/auth/auth-routing.module.ts
@@ -4,27 +4,28 @@ import { LoginComponent } from './login/login.component';
 import { RegistrerComponent } from './registrer/registrer.component';
 
 const routes: Routes = [
-{
-  path: "login",
-  component: LoginComponent,
-  loadChildren: () => import("./login/login.module")
-  .then((refArchivo)=> refArchivo.LoginModule)
-},
-{
-  path: "registrer",
-  component: RegistrerComponent,
-  loadChildren: () => import("./registrer/registrer.module")
-  .then((refArchivo) => refArchivo.RegistrerModule)
-},
-{
-  path: '**',
-  redirectTo: 'auth',
-},
-
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then(
+        (refArchivo) => refArchivo.LoginModule
+      ),
+  },
+  {
+    path: 'registrer',
+    loadChildren: () =>
+      import('./registrer/registrer.module').then(
+        (refArchivo) => refArchivo.RegistrerModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -6,33 +6,27 @@ import { APP_CONFIG } from '../../core/injection-token';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
 export class AuthComponent {
-
   loginForm: FormGroup;
 
-  constructor(
-    public authService: AuthService,
-    private fb: FormBuilder,
-    @Inject(APP_CONFIG) private appConfig: any){
+  constructor(public authService: AuthService, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['test@mail.com', [Validators.required, Validators.email]],
+      password: ['123456', [Validators.required]],
+    });
+  }
 
-      this.loginForm = this.fb.group({
-        email: ["test@mail.com", [Validators.required,Validators.email]],
-        password: ["123456", [Validators.required]],
-      });
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      alert('El formulario no es valido');
+    } else {
+      const data = {
+        email: this.loginForm.get('email')?.value,
+        password: this.loginForm.get('password')?.value,
+      };
+      this.authService.login();
     }
-
-    onSubmit(){
-      if(this.loginForm.invalid){
-        alert("El formulario no es valido");
-      }else{
-        const data = {
-          email: this.loginForm.get("email")?.value,
-          password: this.loginForm.get("password")?.value,
-        };
-        this.authService.login();
-      }
-    }
-
+  }
 }
